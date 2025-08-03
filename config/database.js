ddc31a2e4f9726d5027489a6f842f52d70c65093
Dev Hirpara <devhirpara9999@gmail.com>
2025-08-03 11:30:09 +0530

    db 2

--- config/database.js
+++ config/database.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://devhirpara9999:6ydMz8b1FUAwjabl@cluster0.2rqx39k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://devhirpara9999:6ydMz8b1FUAwjabl@cluster0.2rqx39k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
